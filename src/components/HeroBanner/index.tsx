3530f751fa29f07a79f544fd9b3c79e34235eada
IrisLegend <hiroshitanaka0629@gmail.com>
2023-06-12 23:48:45 -0700

    update: complete with style

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
 
 import BannerTile from '../BannerTile';
 import { BannerPropstype } from 'components/Banner';
 const source = require('../../service/data.json');
+
+const SwiperComponent = styled(Swiper)`
+    & .swiper-button-next, .swiper-button-prev {
+        color: white;
+    }
+`
 
 type SampleData = Pick<BannerPropstype, 'title' | 'description'> & {
     genres: string[];
@@ -13,13 +20,13 @@ type SampleData = Pick<BannerPropstype, 'title' | 'description'> & {
 
 const HeroBanner = () => {
     return <>
-    <Swiper navigation modules={[Navigation]} slidesPerView={1}>
+    <SwiperComponent navigation modules={[Navigation]} slidesPerView={1}>
         {source.data.map((item: SampleData, index: number) => (
             <SwiperSlide key={`key-${item.title}-${index}`}>
                 <BannerTile title={item.title} tags={item.genres} image={item.cover_url} description={item.description} />
             </SwiperSlide>
         ))}
-    </Swiper>
+    </SwiperComponent>
     </>
 }
 
